07547ab591725b28bfa0ed1c4908ff0e67443ce7
Cranberry <annelise.lambin@gmail.com>
2012-10-12 21:27:26 +0200

    Sprint 2 : adapte le SQLManager au connecteur C MySQL

--- server/SQLManager.cpp
+++ server/SQLManager.cpp
@@ -1,5 +1,7 @@
-#include <mysql++/connection.h>
-#include <mysql++/query.h>
+//#include <mysql++/connection.h>
+//#include <mysql++/query.h>
+
+#include <mysql/mysql.h>
 
 #include "SQLManager.h"
 #include "ObjetTest.h"
@@ -10,16 +12,7 @@ SQLManager::SQLManager()
 
 bool SQLManager::launch()
 {
-    mysqlpp::Connection conn(false);
-    if (conn.connect("esa_tfe", "localhost", "root", "mysql"))
-    {
-        std::cout << "CONNEXION OK" << std::endl;
-    }
-    else
-    {
-        std::cout << "ERROR DO CONNEXION" << std::endl;
-    }
-    this->conn = conn;
+    mysql_init(&this->mysql);
 }
 
 SQLManager::SQLManager(const SQLManager& orig)
@@ -28,71 +21,118 @@ SQLManager::SQLManager(const SQLManager& orig)
 
 SQLManager::~SQLManager()
 {
-    this->conn.disconnect();
+    this->close();
+}
+
+void SQLManager::close()
+{
+    mysql_close(&this->mysql);
 }
 
 bool SQLManager::saveData(int format, std::string value, std::string table)
 {
-//    sql_create_2(test, 2, 2, mysqlpp::sql_bigint, format, mysqlpp::sql_mediumtext_null, value);
-            
-    std::cout << "SAVE DATA" << std::endl;
-    
-    test row(mysqlpp::sql_bigint(format), mysqlpp::sql_varchar_null(value));
-    
-    mysqlpp::Query query = this->conn.query();
-    query << "INSERT INTO " << table << " (format, value) VALUES (" << format << ", " << mysqlpp::quote << value << ")";
-//    query.insert(row);
-    query.execute();
-}
-
-std::map<std::string, std::string> SQLManager::getData(int id, std::string table)
-{
-    std::map<std::string, std::string> result;
-    
-    mysqlpp::Query query = this->conn.query();
-    
-    query << "SELECT * FROM " << table << " where id = " << id;
-    
-    mysqlpp::UseQueryResult res = query.use();
-    
-    if (res)
+    if (mysql_real_connect(&this->mysql, "127.0.0.1", "root", "mysql", "esa_tfe", 0, NULL, 0))
     {
-        mysqlpp::Row row = res.fetch_row();
-        result["id"] = row["id"].c_str();
-        result["value"] = row["value"].c_str();
-        result["format"] = row["format"].c_str();
+        std::ostringstream query;
+        query << "INSERT INTO " << table << " (format, value) VALUES ( " << format << ",\"" << value << "\" ) ";
+        std::cout << query.str() << std::endl;
+        mysql_query(&this->mysql, query.str().c_str());
     }
     else
     {
-        std::cerr << "Failed to get stock table: " << query.error() << std::endl;
+        std::cout << "Une erreur s'est produite lors de la connexion à la BDD!";
     }
-    
-    return result;
+}
+
+// TODO : sprintf
+
+std::map<std::string, std::string> SQLManager::getData(int id, std::string table)
+{
+    std::map<std::string, std::string> results;
+    // !! localhost doesn't work on linux, see fav
+    if (mysql_real_connect(&this->mysql, "127.0.0.1", "root", "mysql", "esa_tfe", 0, NULL, 0))
+    {
+        std::ostringstream query;
+        query << "SELECT * FROM " << table << " WHERE id = " << id;
+        std::cout << query.str() << std::endl;
+        mysql_query(&this->mysql, query.str().c_str());
+
+        MYSQL_RES *result = NULL;
+        MYSQL_ROW row;
+        MYSQL_FIELD *fields;
+        unsigned int num_champs = 0;
+
+        num_champs = mysql_field_count(&this->mysql);
+
+        result = mysql_use_result(&this->mysql);
+
+        fields = mysql_fetch_fields(result);
+
+        while ((row = mysql_fetch_row(result)))
+        {
+            unsigned long *lengths;
+
+            lengths = mysql_fetch_lengths(result);
+            for (int i = 0; i < num_champs; i++)
+            {
+                results[fields[i].name] = row[i];
+                //                std::cout << row[i] << std::endl;
+            }
+
+        }
+
+        mysql_free_result(result);
+    }
+    else
+    {
+        std::cout << "Une erreur s'est produite lors de la connexion à la BDD!";
+    }
+
+    return results;
 }
 
 std::vector< std::map< std::string, std::string> > SQLManager::getAllData(std::string table)
 {
     std::vector< std::map< std::string, std::string> > results;
-    mysqlpp::Query query = this->conn.query();
-    query << "SELECT * FROM " << table;
-    
-    mysqlpp::StoreQueryResult res = query.store();
-    
-    if (res)
+    // !! localhost doesn't work on linux, see fav
+    if (mysql_real_connect(&this->mysql, "127.0.0.1", "root", "mysql", "esa_tfe", 0, NULL, 0))
     {
-        for (size_t i = 0; i < res.num_rows(); ++i)
+        std::ostringstream query;
+        query << "SELECT * FROM " << table;
+        std::cout << query.str() << std::endl;
+        mysql_query(&this->mysql, query.str().c_str());
+
+        MYSQL_RES *result = NULL;
+        MYSQL_ROW row;
+        MYSQL_FIELD *fields;
+        unsigned int num_champs = 0;
+
+        num_champs = mysql_field_count(&this->mysql);
+
+        result = mysql_use_result(&this->mysql);
+
+        fields = mysql_fetch_fields(result);
+
+        while ((row = mysql_fetch_row(result)))
         {
-            std::map< std::string, std::string> result;
-            result["id"] = res[i][0].c_str();
-            result["value"] = res[i][1].c_str();
-            result["format"] = res[i][2].c_str();
-            results.push_back(result);
+            unsigned long *lengths;
+
+            lengths = mysql_fetch_lengths(result);
+            for (int i = 0; i < num_champs; i++)
+            {
+                std::map< std::string, std::string> result;
+                result[fields[i].name] = row[i];
+                results.push_back(result);
+            }
+
         }
+
+        mysql_free_result(result);
     }
     else
     {
-        std::cerr << "Failed to get stock table: " << query.error() << std::endl;
+        std::cout << "Une erreur s'est produite lors de la connexion à la BDD!";
     }
-    
+
     return results;
 }
--- server/SQLManager.h
+++ server/SQLManager.h
@@ -7,6 +7,8 @@
 #include <map>
 #include <vector>
 #include <ssqls.h>
+#include <string>
+#include <sstream>
 
 class SQLManager {
 public:
@@ -17,8 +19,10 @@ public:
     bool saveData(int format, std::string value, std::string table);
     std::map<std::string, std::string> getData(int id, std::string table);
     std::vector< std::map< std::string, std::string> > getAllData(std::string table);
+    void close();
 private:
     mysqlpp::Connection conn;
+    MYSQL mysql;
 };
 
 #endif	/* SQLMANAGER_H */
